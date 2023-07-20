aaf53a22057777eb4125070a143afe7a3596be2c
Florian <flo.minion.info@gmail.com>
2023-07-20 19:01:34 +0200

    Applying input check

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -25,16 +25,15 @@ class RowChecker:
     """
 
     VALID_FORMATS = (
-        ".fq.gz",
-        ".fastq.gz",
+        ".tiff",
     )
 
     def __init__(
         self,
         sample_col="sample",
-        first_col="fastq_1",
-        second_col="fastq_2",
-        single_col="single_end",
+        first_col="channel",
+        second_col="tiff",
+        #single_col="single_end",
         **kwargs,
     ):
         """
@@ -43,20 +42,20 @@ class RowChecker:
         Args:
             sample_col (str): The name of the column that contains the sample name
                 (default "sample").
-            first_col (str): The name of the column that contains the first (or only)
-                FASTQ file path (default "fastq_1").
-            second_col (str): The name of the column that contains the second (if any)
-                FASTQ file path (default "fastq_2").
-            single_col (str): The name of the new column that will be inserted and
-                records whether the sample contains single- or paired-end sequencing
-                reads (default "single_end").
+            first_col (str): The name of the column that contains the channel name
+                (default "channel").
+            second_col (str): The name of the column that contains the tiff file
+                Tiff file path (default "tiff").
+            #single_col (str): The name of the new column that will be inserted and
+            #    records whether the sample contains single- or paired-end sequencing
+            #    reads (default "single_end").
 
         """
         super().__init__(**kwargs)
         self._sample_col = sample_col
         self._first_col = first_col
         self._second_col = second_col
-        self._single_col = single_col
+        #self._single_col = single_col
         self._seen = set()
         self.modified = []
 
@@ -84,15 +83,16 @@ class RowChecker:
         row[self._sample_col] = row[self._sample_col].replace(" ", "_")
 
     def _validate_first(self, row):
-        """Assert that the first FASTQ entry is non-empty and has the right format."""
+        """Assert that the channel entry is non-empty and has the right format."""
         if len(row[self._first_col]) <= 0:
-            raise AssertionError("At least the first FASTQ file is required.")
-        self._validate_fastq_format(row[self._first_col])
+            raise AssertionError("Channel name required.")
+        row[self._first_col] = row[self._first_col].replace(" ", "_")
 
     def _validate_second(self, row):
-        """Assert that the second FASTQ entry has the right format if it exists."""
-        if len(row[self._second_col]) > 0:
-            self._validate_fastq_format(row[self._second_col])
+        """Assert that the tiff entry has the right format if it exists."""
+        if len(row[self._second_col]) <= 0:
+            raise AssertionError("Tiff required.")
+        self._validate_tiff_format(row[self._second_col])
 
     def _validate_pair(self, row):
         """Assert that read pairs have the same file extension. Report pair status."""
@@ -105,24 +105,24 @@ class RowChecker:
         else:
             row[self._single_col] = True
 
-    def _validate_fastq_format(self, filename):
-        """Assert that a given filename has one of the expected FASTQ extensions."""
+    def _validate_tiff_format(self, filename):
+        """Assert that a given filename has tiff extension."""
         if not any(filename.endswith(extension) for extension in self.VALID_FORMATS):
             raise AssertionError(
-                f"The FASTQ file has an unrecognized extension: {filename}\n"
+                f"The tiff file has an unrecognized extension: {filename}\n"
                 f"It should be one of: {', '.join(self.VALID_FORMATS)}"
             )
 
     def validate_unique_samples(self):
         """
-        Assert that the combination of sample name and FASTQ filename is unique.
+        Assert that the combination of sample name and tiff filename is unique.
 
         In addition to the validation, also rename all samples to have a suffix of _T{n}, where n is the
-        number of times the same sample exist, but with different FASTQ files, e.g., multiple runs per experiment.
+        number of times the same sample exist, but with different tiff files, e.g., multiple runs per experiment.
 
         """
         if len(self._seen) != len(self.modified):
-            raise AssertionError("The pair of sample name and FASTQ must be unique.")
+            raise AssertionError("The pair of channel and tiff must be unique.")
         seen = Counter()
         for row in self.modified:
             sample = row[self._sample_col]
@@ -166,8 +166,8 @@ def check_samplesheet(file_in, file_out):
     """
     Check that the tabular samplesheet has the structure expected by nf-core pipelines.
 
-    Validate the general shape of the table, expected columns, and each row. Also add
-    an additional column which records whether one or two FASTQ reads were found.
+    Validate the general shape of the table, expected columns, and each row. 
+    # Also add an additional column which records whether one or two FASTQ reads were found.
 
     Args:
         file_in (pathlib.Path): The given tabular samplesheet. The format can be either
@@ -179,16 +179,16 @@ def check_samplesheet(file_in, file_out):
         This function checks that the samplesheet follows the following structure,
         see also the `viral recon samplesheet`_::
 
-            sample,fastq_1,fastq_2
-            SAMPLE_PE,SAMPLE_PE_RUN1_1.fastq.gz,SAMPLE_PE_RUN1_2.fastq.gz
-            SAMPLE_PE,SAMPLE_PE_RUN2_1.fastq.gz,SAMPLE_PE_RUN2_2.fastq.gz
-            SAMPLE_SE,SAMPLE_SE_RUN1_1.fastq.gz,
+            sample,channel,tiff
+            SAMPLE,001,exemplar-001-cycle-08.ome.tiff
+            SAMPLE,001,exemplar-001-cycle-07.ome.tiff
+            SAMPLE,001,exemplar-001-cycle-06.ome.tiff
 
-    .. _viral recon samplesheet:
-        https://raw.githubusercontent.com/nf-core/test-datasets/viralrecon/samplesheet/samplesheet_test_illumina_amplicon.csv
+    #.. _viral recon samplesheet:
+    #    https://raw.githubusercontent.com/nf-core/test-datasets/viralrecon/samplesheet/samplesheet_test_illumina_amplicon.csv
 
     """
-    required_columns = {"sample", "fastq_1", "fastq_2"}
+    required_columns = {"sample", "channel", "tiff"}
     # See https://docs.python.org/3.9/library/csv.html#id3 to read up on `newline=""`.
     with file_in.open(newline="") as in_handle:
         reader = csv.DictReader(in_handle, dialect=sniff_format(in_handle))
--- subworkflows/local/input_check.nf
+++ subworkflows/local/input_check.nf
@@ -13,10 +13,10 @@ workflow INPUT_CHECK {
         .csv
         .splitCsv ( header:true, sep:',' )
         .map { create_fastq_channel(it) }
-        .set { reads }
+        .set { images }
 
     emit:
-    reads                                     // channel: [ val(meta), [ reads ] ]
+    images                                     // channel: [ val(meta), [ reads ] ]
     versions = SAMPLESHEET_CHECK.out.versions // channel: [ versions.yml ]
 }
 
@@ -25,20 +25,21 @@ def create_fastq_channel(LinkedHashMap row) {
     // create meta map
     def meta = [:]
     meta.id         = row.sample
-    meta.single_end = row.single_end.toBoolean()
+    meta.tiff = row.single_end.toBoolean()
 
     // add path(s) of the fastq file(s) to the meta map
     def fastq_meta = []
-    if (!file(row.fastq_1).exists()) {
-        exit 1, "ERROR: Please check input samplesheet -> Read 1 FastQ file does not exist!\n${row.fastq_1}"
+    if (!file(row.tiff).exists()) {
+        exit 1, "ERROR: Please check input samplesheet -> tiff file does not exist!\n${row.tiff}"
     }
-    if (meta.single_end) {
-        fastq_meta = [ meta, [ file(row.fastq_1) ] ]
-    } else {
-        if (!file(row.fastq_2).exists()) {
-            exit 1, "ERROR: Please check input samplesheet -> Read 2 FastQ file does not exist!\n${row.fastq_2}"
-        }
-        fastq_meta = [ meta, [ file(row.fastq_1), file(row.fastq_2) ] ]
-    }
+    // if (meta.single_end) {
+    //     fastq_meta = [ meta, [ file(row.fastq_1) ] ]
+    // } else {
+    //     if (!file(row.fastq_2).exists()) {
+    //         exit 1, "ERROR: Please check input samplesheet -> Read 2 FastQ file does not exist!\n${row.fastq_2}"
+    //     }
+    //}
+    fastq_meta = [ meta, [ file(row.tiff) ] ]
+
     return fastq_meta
 }
