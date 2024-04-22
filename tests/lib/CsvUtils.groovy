8e9378627cf03155e9e08e64f8a11eb67c570a48
Jeremy Muhlich <jmuhlich@bitflood.org>
2024-04-22 16:50:40 -0400

    Parameterize rounding precision in CsvUtils

--- tests/lib/CsvUtils.groovy
+++ tests/lib/CsvUtils.groovy
@@ -5,24 +5,46 @@ import com.opencsv.CSVWriter
 import com.askimed.nf.test.util.ObjectUtil
 import java.nio.file.Paths
 
-static String roundAndHashCsv(String path) {
+static String roundAndHashCsv(String path, int precision = 12) {
     def name = Paths.get(path).getFileName().toString()
-    def reader = new CSVReader(new File(path).newReader())
-    def strWriter = new StringWriter()
-    def writer = new CSVWriter(strWriter)
-    def row
-    while (row = reader.readNext()) {
-        row = row.collect(this.&roundIfDouble)
-        writer.writeNext(row as String[], false)
-    }
-    def csvContent = strWriter.toString()
+    def csvContent = roundCsv(path, precision)
     return name + ":rounded:md5," + ObjectUtil.getMd5(csvContent)
 }
 
-// Parse and round floating point values to 12 decimal places. Pass other
-// strings through untouched.
-static String roundIfDouble(String value) {
-    value.contains(".")
-    ? (Math.round(Double.parseDouble(value) * 1e12) / 1e12).toString()
+static String roundCsv(String path, int precision) {
+    def reader = new CSVReader(new File(path).newReader())
+    def strWriter = new StringWriter()
+    def writer = new CSVWriter(strWriter)
+    // Copy header row.
+    writer.writeNext(reader.readNext(), false)
+    def row
+    while (row = reader.readNext()) {
+        row = row.collect{ roundIfDouble(it, precision) }
+        writer.writeNext(row as String[], false)
+    }
+    def csvContent = strWriter.toString()
+    return csvContent
+}
+
+static String summarizeCsv(String path) {
+    def reader = new CSVReader(new File(path).newReader())
+    def strWriter = new StringWriter()
+    def headers = reader.readNext() as List
+    def count = 0
+    while (reader.readNext()) {
+        count++
+    }
+    return [
+        headers: headers,
+        rowCount: count,
+    ]
+}
+
+// Parse and round floating point values to the specified number of decimal
+// digits of precision. Pass other strings through untouched.
+static String roundIfDouble(String value, int precision) {
+    assert precision > 0
+    return value.contains(".")
+    ? sprintf("%.${precision}g", Double.parseDouble(value))
     : value
 }
